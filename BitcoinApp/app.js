c927debace64c25294138524704725316b7b9dfb
Callum S Clarke <callum.clarke@live.co.uk>
2021-07-31 14:40:09 +0100

    fix port

--- BitcoinApp/app.js
+++ BitcoinApp/app.js
@@ -10,7 +10,7 @@ app.get("/", function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
-serv.listen(2000);
+serv.listen(process.env.PORT);
 
 console.log("Server has started...");
 
